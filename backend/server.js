13be1945e0747242dc973b04c7ba38758903102c
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-10-08 09:06:02 +0530

    Added error handler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/db");
+const { errorMiddleware, notFound } = require("./middleware/errorMiddleware");
 const gamerRouter = require("./routes/gamer");
 const PORT = process.env.PORT;
 
@@ -15,6 +16,10 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 app.use(gamerRouter);
+
+app.use(errorMiddleware);
+app.use(notFound);
+
 app.listen(PORT, () =>
 	console.log(`Server running at http://127.0.0.1:${PORT}`),
 );
