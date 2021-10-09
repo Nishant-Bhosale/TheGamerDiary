3609ded0ccfcd05c9864504b64cffc076d4d1052
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-10-09 10:02:13 +0530

    Changed format of date

--- backend/controllers/gamer.js
+++ backend/controllers/gamer.js
@@ -7,7 +7,7 @@ const addGamer = asyncHandler(async (req, res) => {
 	const { name, money, amountOfTime, pcId, gamerId } = req.body;
 	const gamer = await Gamer.findOne({ _id: gamerId });
 
-	if (gamer && gamer.createdOn === moment().format('DD/MM/YYYY')) {
+	if (gamer && gamer.createdOn === moment().format('MM/DD/YYYY')) {
 		//Conditional will execute if gamerId is sent
 		gamer.endTime += amountOfTime * 60 * 1000;
 		gamer.totalMoneyPaid += money;
@@ -31,7 +31,7 @@ const addGamer = asyncHandler(async (req, res) => {
 			isPlaying: true,
 			totalMoneyPaid: money,
 			totalTime: amountOfTime,
-			createdOn: moment().format('DD/MM/YYYY')
+			createdOn: moment().format('MM/DD/YYYY'),
 		});
 
 		const pc = await PC.findOne({ pcNumber: pcId });
